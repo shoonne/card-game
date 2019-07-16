10b253401f2521636c9a07960544fd01127771f8
shoonne <shaon_ahmed@msn.com>
2019-07-16 18:08:48 +0200

    You can now draw a card from the deck

--- App.js
+++ App.js
@@ -1,18 +1,74 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props)
   
     this.state = {
-       
+      dataSource: [],
+      succsess: false,
+      deckID: null,
+      drawnCard: null,
+      cardsRemaining: null
     };
+
+    this.drawCardAsync = this.drawCardAsync.bind(this);
   };
+
+  componentDidMount() {
+    this.getDecksFromApiAsync();
+  }
+
+  getDecksFromApiAsync() {
+    return fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          succsess: responseJson.succsess, 
+          dataSource: responseJson,
+          deckID: responseJson.deck_id
+        })
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  drawCardAsync = () => {
+    const DRAW_CARD_URL = `https://deckofcardsapi.com/api/deck/${this.state.deckID}/draw/?count=1`;
+
+    return fetch(DRAW_CARD_URL)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({
+        drawnCard: responseJson,
+        cardsRemaining: responseJson.remaining
+      });
+      //console.log(this.state.deckID)
+    })
+    .catch((error) =>{
+      console.log(error)
+    });
+  }
+  
   render() {
+
+    const data = this.state.dataSource;
+    const cardsRemaining = this.state.cardsRemaining;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+          <Text>Remaining cards: {cardsRemaining ? cardsRemaining : 52}</Text>
+      </View>
+      <TouchableOpacity onPress={() => {console.log(this.state.drawnCard)}}>
+        <Text>SHOW CARD</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={this.drawCardAsync}>
+        <Text>DRAW CARD</Text>
+      </TouchableOpacity>
     </View>
   );
   }
